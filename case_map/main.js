91ce20cc719f7900dc57f15b9ccf551a788b1b66
Ting <evajkl2342@gmaiil.com>
2020-04-22 16:59:37 +0800

    add data.json

--- case_map/main.js
+++ case_map/main.js
@@ -1,69 +1,185 @@
-window.app = {};
-var sidebar = new ol.control.Sidebar({ element: 'sidebar', position: 'right' });
-
-var projection = ol.proj.get('EPSG:3857');
-var projectionExtent = projection.getExtent();
-var size = ol.extent.getWidth(projectionExtent) / 256;
-var resolutions = new Array(20);
-var matrixIds = new Array(20);
-var clickedCoordinate, populationLayer, gPopulation;
-for (var z = 0; z < 20; ++z) {
-    // generate resolutions and matrixIds arrays for this WMTS
-    resolutions[z] = size / Math.pow(2, z);
-    matrixIds[z] = z;
-}
-
-var baseLayer = new ol.layer.Tile({
-    source: new ol.source.WMTS({
-        matrixSet: 'EPSG:3857',
-        format: 'image/png',
-        url: 'http://wmts.nlsc.gov.tw/wmts',
-        layer: 'EMAP',
-        tileGrid: new ol.tilegrid.WMTS({
-            origin: ol.extent.getTopLeft(projectionExtent),
-            resolutions: resolutions,
-            matrixIds: matrixIds
-        }),
-        style: 'default',
-        wrapX: true,
-        attributions: '<a href="http://maps.nlsc.gov.tw/" target="_blank">國土測繪圖資服務雲</a>'
-    }),
-    opacity: 0.3
-});
-
-var appView = new ol.View({
-  center: ol.proj.fromLonLat([120.221115, 22.964407]),
-  zoom: 14
-});
-var datalayer=new ol.layer.Vector({
-  source:new ol.source.Vector({
-    url:'../data.json',format:new ol.
-    format.GeoJSON()
-  })
-});
-var map = new ol.Map({
-  layers: [baseLayer,datalayer],
-  target: 'map',
-  view: appView
-});
-map.addControl(sidebar);
-
-var content = document.getElementById('sidebarContent');
-map.on('singleclick', function(evt) {
-  content.innerHTML = '';
-  clickedCoordinate = evt.coordinate;
-
-  map.forEachFeatureAtPixel(evt.pixel, function (feature, layer) {
-    var message = '';
-    var p = feature.getProperties();
-    for(k in p) {
-      if(k !== 'geometry') {
-        message += k + ': ' + p[k] + '<br />';
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              120.54473876953125,
+              23.792884472869158
+            ],
+            [
+              120.61614990234374,
+              23.387640227334956
+            ],
+            [
+              120.77270507812499,
+              23.61181198048863
+            ],
+            [
+              120.54473876953125,
+              23.792884472869158
+            ]
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              121.33300781249999,
+              24.696934226366672
+            ],
+            [
+              121.14624023437499,
+              24.26699728841817
+            ],
+            [
+              121.46484375,
+              24.206889622398023
+            ],
+            [
+              121.33300781249999,
+              24.696934226366672
+            ]
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          121.168212890625,
+          23.38259828417886
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          120.92651367187499,
+          23.150462029224087
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          120.55297851562499,
+          23.0999442125314
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          120.70678710937499,
+          22.79643932091949
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              121.124267578125,
+              23.003908026630658
+            ],
+            [
+              120.84686279296874,
+              22.669778674332314
+            ],
+            [
+              121.16546630859375,
+              22.91033339315192
+            ],
+            [
+              121.124267578125,
+              23.003908026630658
+            ]
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          120.81665039062499,
+          24.046463999666567
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          121.51977539062499,
+          24.836595553891183
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          121.10229492187501,
+          23.976214626638292
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          120.355224609375,
+          23.674712836608773
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          120.28930664062499,
+          23.473323877771172
+        ]
       }
     }
-
-    content.innerHTML += message + '<hr />';
-  });
-
-  sidebar.open('home');
-});
+  ]
+}
